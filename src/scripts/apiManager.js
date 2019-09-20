5c1d09dbd66c3620bc2d03f7561a3b7e001a513f
LanceP51 <appleguy05@live.com>
2019-09-20 13:54:59 -0400

    completed practices

---
+++ src/scripts/apiManager.js
@@ -0,0 +1,4 @@
+// select, and add event listerner to sve button and console log the entered value
+document.querySelector("#nameSubmit").addEventListener("click",function (){
+    console.log(document.querySelector("#studentName").value)
+})
